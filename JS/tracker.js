d66db8a8ab55308d15e660067be4d8a872f49d93
Kristin Aldridge <kristinaldridge202@gmail.com>
2024-05-09 10:57:38 -0400

    added div,li, and textnode

--- JS/tracker.js
+++ JS/tracker.js
@@ -1,28 +1,25 @@
-document.body.onload = addElement;
-
-function addElement() {
-
-let header = body.createElement("div");
-let textNode = body.createTextNode("Tally Up!");
-header.appendChild(textNode);
-
-const currentDiv = document.getElementById("div1")
-
-document.body.insertBefore(header, currentDiv);
-
-}
+alert("success")
 
 
-const liList = doument.getElementById("commentList")
-const textInput = documnet.getElementById("textComment")
-const textButton = document.getElementById("buttonAdd")
+const newDiv = document.createElement("div");
+const textNode = document.createTextNode("Tally Up!");
+const newListItem = document.createElement("li")
+newDiv.appendChild(textNode);
+newDiv.appendChild(newListItem);
+document.body.appendChild(newDiv);
 
-function addComment (){
-    const newComment = liListList.value;
-    if (newComment === "") return;
-    liListList.appendChild(document.createElement("li")).textcontent = newComment;
-    textInput.value ="";
-    textInput.focus();
-}
 
-textButton.addEventListener("click", addComment);
+
+//const liList = doument.getElementById("commentList")
+//const textInput = documnet.getElementById("textComment")
+////const textButton = document.getElementById("buttonAdd")
+
+//function addComment (){
+   // const newComment = liListList.value;
+   // if (newComment === "") return;
+   // liListList.appendChild(document.createElement("li")).textcontent = newComment;
+   // textInput.value ="";
+   // textInput.focus();
+//}
+
+//textButton.addEventListener("click", addComment);
